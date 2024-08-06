253c71300f36ee9000d9765dad0cbd5c0fd0cb1f
novafacing <rowanbhart@gmail.com>
2024-08-05 18:13:40 -0700

    Remove examples

--- examples/snudown/snudownrust.rs
+++
@@ -1,9 +0,0 @@
-// TODO(kkysen) `extern crate`s shouldn't be needed, but not sure how to test this
-#[cfg(feature="cross-check")]
-extern crate c2rust_xcheck_derive;
-#[cfg(feature="cross-check")]
-extern crate c2rust_xcheck_runtime;
-extern crate autolink;
-extern crate buffer;
-extern crate markdown;
-extern crate stack;
